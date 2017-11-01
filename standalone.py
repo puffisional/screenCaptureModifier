ce04e78fba095b5a3fcd1a44c1c26278c638a603
dodik <j.c.bednarcik@gmail.com>
2017-11-01 13:21:15 +0100

    Added argparse into standalone version

--- standalone.py
+++ standalone.py
@@ -9,14 +9,23 @@ from PyQt4.QtGui import QApplication
 from screenCaptureModifier import mainWindow
 from screenCaptureModifier.screenCapture import ScreenCapture
 from PyQt4.Qt import QRect
+import argparse
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     
+    p = argparse.ArgumentParser()
+    p.add_argument("--coords", default="200,200,800,600")
+    p.add_argument("--fps", default=15)
+    args = p.parse_args()
+    
     window = mainWindow.MainWindow()
     window.show()
+    
+       
     # Tu si vyberas coordinaty na grabovanie plochy a fps
-    capture = ScreenCapture(coords=QRect(300,300,512,512), fps=15)
+    x,y,width,height = args.coords.split(",")
+    capture = ScreenCapture(coords=QRect(int(x), int(y), int(width), int(height)), fps=int(args.fps))
     
     capture.newScreen.connect(window.originalPictureWidget.updateImage)
     capture.newTransformedScreen.connect(window.transofrmedPictureWidget.updateImage)
